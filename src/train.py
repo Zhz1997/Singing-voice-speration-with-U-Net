d2354fce4e22ad1bdd0268008bcc4a67ea7761ca
Zhz1997 <zhz19970704@hotmail.com>
2020-11-07 19:53:25 -0800

    Modify train.py so that it pass data to unet.py to build model

--- src/train.py
+++ src/train.py
@@ -2,6 +2,10 @@ import librosa
 from librosa.util import find_files
 import numpy as np
 from unet import unet
+import tensorflow as tf
+from config import *
+
+patchSize = 128
 
 def readNpzToLists(filePath):
     fileList = find_files(filePath, ext="npz")
@@ -16,14 +20,42 @@ def readNpzToLists(filePath):
 
     return mixList, instruList, vocalList 
 
+def getSampleFromList(mixMagList,targetMagList) :
+    mixSampleList = []
+    targetSampleList = []
+    for mix, target in zip(mixMagList,targetMagList) :
+        starts = np.random.randint(0, mix.shape[1] - patchSize, (mix.shape[1] - patchSize) // 10)
+        for start in starts:
+            end = start + patchSize
+            mixSampleList.append(mix[1:, start:end, np.newaxis])
+            targetSampleList.append(target[1:, start:end, np.newaxis])
+    return np.asarray(mixSampleList, dtype=np.float32), np.asarray(targetSampleList, dtype=np.float32)
+
+def getMag(specList):
+    result = []
+    for s in specList:
+        mag, _ = librosa.magphase(s)
+        result.append(mag)
+    return result
+
 
 if __name__ == '__main__':
     # load DSD100_Npz
     mixListD, instruListD, vocalListD = readNpzToLists("../DSD100_Npz/Dev")
     # load MedlyDb_Npz
     mixListM, instruListM, vocalListM = readNpzToLists("../MedleyDB_Npz")
+
     mixList = mixListD + mixListM
     vocalList = vocalListD + vocalListM
     instruList = instruListD + instruListM
-    
-    unet(mixList, vocalList)
+
+    mixMagList = getMag(mixList)
+    vocalMagList = getMag(vocalList)
+    instruMagList = getMag(instruList)
+
+
+    mix, target = getSampleFromList(mixMagList, vocalMagList)
+    unetModel = tf.estimator.Estimator(model_fn=unet, model_dir="./model")
+    inputFn = tf.compat.v1.estimator.inputs.numpy_input_fn(x = {"mix":mix}, y = target, batch_size=16, shuffle=False, num_epochs=40)
+    unetModel.train(input_fn = inputFn)
+
